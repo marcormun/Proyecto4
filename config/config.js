16cbf54b97d94ff341bc17f63c01759ddcff67c9
marcormun <mcordontrabajo@gmail.com>
2022-06-17 17:25:20 +0200

    Seeders creados, config.js modificado

--- config/config.js
+++ config/config.js
@@ -3,11 +3,11 @@ require('dotenv').config();
 module.exports = {
 
   environment: process.env.NODE_ENV || 'development',
-  username: process.env.MYSQL_USER || 'remote',
-  password: process.env.MYSQL_PASSWORD || 'root',
-  database: process.env.MYSQL_DATABASE || 'proyecto4_db',
-  host: process.env.MYSQL_HOST || '192.168.1.107',
-  port: process.env.MYSQL_PORT || 3306,
+  username: process.env.MYSQL_USER,
+  password: process.env.MYSQL_PASSWORD,
+  database: process.env.MYSQL_DATABASE,
+  host: process.env.MYSQL_HOST,
+  port: 3306,
   dialect: process.env.DIALECT || 'mysql'
 
 }
